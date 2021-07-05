2ddd07bdea98639b1de1bf37c349bb6f1ab42664
kumamgane <m1861008@maebashi-it.ac.jp>
2021-07-05 14:29:50 +0900

    [Fix] C078

--- C078.php
+++ C078.php
@@ -10,15 +10,18 @@ for ($i = 0; $i < $date; $i++) {
     $cost[$i] = trim(fgets(STDIN));
 }
 
-for ($i = 0; $i < $date - 1; $i++) {
-    if ($cost[$i] <= $buy){
-        $profit -=$cost[$i];
+$tmp = $date - 1;
+for ($i = 0; $i < $tmp; $i++) {
+    if ($cost[$i] <= $buy) {
+        $profit -= $cost[$i];
         $items++;
-    }elseif ($cost[$i] >= $sell) {
-        $profit +=$cost[$i]*$items;
+        continue;
+    }
+    if ($cost[$i] >= $sell) {
+        $profit += $cost[$i] * $items;
         $items = 0;
     }
 }
-$profit +=$cost[$date-1]*$items;
+$profit += $cost[$date - 1] * $items;
 
 echo $profit . PHP_EOL;

7d9fc12a14a51300edee8106320ec42ae6c7b613
kumamgane <m1861008@maebashi-it.ac.jp>
2021-07-05 14:18:43 +0900

    [Fix] C017

--- C017.php
+++ C017.php
@@ -1,13 +1,11 @@
 <?php
-$parent = trim(fgets(STDIN));
-$parent = explode(' ', $parent);
+$parent = explode(' ', trim(fgets(STDIN)));
 
 $inputLine = trim(fgets(STDIN));
 
 $children = [];
 for ($i = 0; $i < $inputLine; $i++) {
-    $children[$i] = trim(fgets(STDIN));
-    $children[$i] = explode(' ', $children[$i]);
+    $children[$i] = explode(' ', trim(fgets(STDIN)));
 }
 
 $judg = ['High', 'Low'];
@@ -15,15 +13,18 @@ $answer = [];
 for ($i = 0; $i < $inputLine; $i++) {
     if ($children[$i][0] < $parent[0]) {
         $answer[$i] = $judg[0];
-    } elseif ($children[$i][0] == $parent[0]) {
-        if ($children[$i][1] > $parent[1]) {
-            $answer[$i] = $judg[0];
-        } else {
-            $answer[$i] = $judg[1];
-        }
-    } else {
-        $answer[$i] = $judg[1];
+        continue;
     }
+    if ($children[$i][0] > $parent[0]) {
+        $answer[$i] = $judg[1];
+        continue;
+    }
+
+    if($children[$i][1] > $parent[1]){
+        $answer[$i] = $judg[0];
+        continue;
+    }
+    $answer[$i] = $judg[1];
 }
 
 for ($i = 0; $i < $inputLine; $i++) {
